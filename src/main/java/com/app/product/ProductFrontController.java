49abff6fd97ea18ff1d157423ba257577421c4a2
dev-jane99 <ruejaeeun99@gmail.com>
2024-10-15 15:45:35 +0900

    최초 push

---
+++ src/main/java/com/app/product/ProductFrontController.java
@@ -0,0 +1,46 @@
+package com.app.product;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.app.Result;
+
+public class ProductFrontController extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=utf-8");
+		String target = req.getRequestURI().replace(req.getContextPath() + "/", "").split("\\.")[0];
+		Result result = null;
+		
+		if(target.equals("write")) {
+		}else if(target.equals("write-ok")) {
+			
+		}else if(target.equals("list")) {
+			
+		}else if(target.equals("delete-ok")) {
+			
+		}else {
+//			전부 notFound 404
+		}
+		
+		if(result != null) {
+			if(result.isRedirect()){
+//				redirect
+				resp.sendRedirect(result.getPath());
+			}else {
+//				forward
+				req.getRequestDispatcher(result.getPath()).forward(req, resp);
+			}
+		}
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
