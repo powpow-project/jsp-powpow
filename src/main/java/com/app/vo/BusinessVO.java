e5f437b41f06f8d0db48f87ac114aa1aecfbac6e
soyeon-00 <soyeonlee0515@gmail.com>
2024-10-21 15:03:26 +0900

    soyeon

--- src/main/java/com/app/vo/BusinessVO.java
+++ src/main/java/com/app/vo/BusinessVO.java
@@ -1,5 +1,69 @@
 package com.app.vo;
 
-public class BusinessVO {
+import java.util.Objects;
 
+public class BusinessVO {
+    private Long id;
+    private String businessName;
+    private String businessRepresentativeName;
+    private String businessNumber;
+    
+    public BusinessVO() {;}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBusinessName() {
+		return businessName;
+	}
+
+	public void setBusinessName(String businessName) {
+		this.businessName = businessName;
+	}
+
+	public String getBusinessRepresentativeName() {
+		return businessRepresentativeName;
+	}
+
+	public void setBusinessRepresentativeName(String businessRepresentativeName) {
+		this.businessRepresentativeName = businessRepresentativeName;
+	}
+
+	public String getBusinessNumber() {
+		return businessNumber;
+	}
+
+	public void setBusinessNumber(String businessNumber) {
+		this.businessNumber = businessNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "BusinessVO [id=" + id + ", businessName=" + businessName + ", businessRepresentativeName="
+				+ businessRepresentativeName + ", businessNumber=" + businessNumber + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BusinessVO other = (BusinessVO) obj;
+		return Objects.equals(id, other.id);
+	}
+
+    
 }
