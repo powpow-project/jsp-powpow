b0b9f19fc84ff0768f7e5f5456bdda9f590dcf64
str-taehyeok <gimtaehyeog63@gmail.com>
2024-10-23 15:03:30 +0900

    seller controller 변경

--- src/main/java/com/app/seller/controller/SellerDeleteOkController.java
+++ src/main/java/com/app/seller/controller/SellerDeleteOkController.java
@@ -1,7 +1,6 @@
-package com.app.product.controller;
+package com.app.seller.controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,22 +9,18 @@ import javax.servlet.http.HttpServletResponse;
 import com.app.Action;
 import com.app.Result;
 import com.app.dao.ProductDAO;
-import com.app.vo.ProductVO;
 
-public class ProductListController implements Action {
+public class SellerDeleteOkController implements Action {
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		Result result = new Result();
 		ProductDAO productDAO = new ProductDAO();
-
-        
-	
-	    List<ProductVO> products = productDAO.selectAll();
-	    System.out.println(products);
-	    req.setAttribute("products", products);
-
-	    result.setPath("../product/product-list.jsp");
+		
+		productDAO.delete(Long.parseLong(req.getParameter("id")));
+		
+		result.setRedirect(true);
+		result.setPath("seller-list.seller");
 		
 		return result;
 	}
