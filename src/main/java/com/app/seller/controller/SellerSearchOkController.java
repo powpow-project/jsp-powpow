1e9aab63a359c91653a96bc769822060832dbf78
str-taehyeok <gimtaehyeog63@gmail.com>
2024-10-29 17:46:20 +0900

    매출조회 중간

--- src/main/java/com/app/seller/controller/SellerSearchOkController.java
+++ src/main/java/com/app/seller/controller/SellerSearchOkController.java
@@ -1,29 +1,37 @@
-/*
- * package com.app.seller.controller;
- * 
- * import java.io.IOException;
- * 
- * import javax.servlet.ServletException; import
- * javax.servlet.http.HttpServletRequest; import
- * javax.servlet.http.HttpServletResponse;
- * 
- * import com.app.Action; import com.app.Result;
- * 
- * public class SellerSearchOkController implements Action {
- * 
- * @Override public Result execute(HttpServletRequest req, HttpServletResponse
- * resp) throws IOException, ServletException { Result result = new Result();
- * 
- * String startDate = req.getParameter("start-date"); String endDate =
- * req.getParameter("end-date");
- * 
- * req.setAttribute("startDate", startDate); req.setAttribute("endDate",
- * endDate);
- * 
- * result.setRedirect(true);
- * result.setPath("../seller/seller-revenue-list.seller");
- * 
- * return result; }
- * 
- * }
- */
+package com.app.seller.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.app.Action;
+import com.app.Result;
+import com.app.dao.SellerDAO;
+
+public class SellerSearchOkController implements Action {
+
+	@Override
+	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		Result result = new Result();
+		String startDate = req.getParameter("start-date");
+        String endDate = req.getParameter("end-date");
+        HttpSession session = req.getSession();
+        SellerDAO sellerDAO = new SellerDAO();
+        
+//      String sellerEmail = (String)session.getAttribute("sellerEmail");
+        String sellerEmail = "abc123";
+        Long sellerId = sellerDAO.selectBySellerEmail(sellerEmail).getId();
+        
+        req.setAttribute("startDate", startDate);
+        req.setAttribute("endDate", endDate);
+		
+		result.setRedirect(true);
+		result.setPath("../seller/seller-revenue-list.seller");
+		
+		return result;
+	}
+
+}
